4890b53e645fdc537a9697fff11d12e6e413831e
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-09 07:22:08 +0500

    Добавил проверку статуса сети

--- app/src/main/java/com/alex44/digitalnomadstestapp/di/modules/RepoModule.kt
+++ app/src/main/java/com/alex44/digitalnomadstestapp/di/modules/RepoModule.kt
@@ -1,5 +1,6 @@
 package com.alex44.digitalnomadstestapp.di.modules
 
+import com.alex44.digitalnomadstestapp.common.interfaces.INetworkStatus
 import com.alex44.digitalnomadstestapp.model.api.INewsSource
 import com.alex44.digitalnomadstestapp.model.repo.INewsRepo
 import com.alex44.digitalnomadstestapp.model.repo.INewsRepoCache
@@ -8,10 +9,13 @@ import dagger.Module
 import dagger.Provides
 import javax.inject.Named
 
-@Module(includes = [ApiModule::class, CacheModule::class])
+@Module(includes = [ApiModule::class, CacheModule::class, NetworkModule::class])
 class RepoModule {
 
     @Provides
-    fun newsRepo(newsSource : INewsSource, @Named("Room") repoCache : INewsRepoCache) : INewsRepo = NewsRepo(newsSource, repoCache)
+    fun newsRepo(newsSource : INewsSource,
+                 @Named("Room") repoCache : INewsRepoCache,
+                 networkStatus: INetworkStatus
+    ) : INewsRepo = NewsRepo(newsSource, repoCache, networkStatus)
 
 }
--- app/src/main/java/com/alex44/digitalnomadstestapp/model/repo/NewsRepo.kt
+++ app/src/main/java/com/alex44/digitalnomadstestapp/model/repo/NewsRepo.kt
@@ -1,20 +1,34 @@
 package com.alex44.digitalnomadstestapp.model.repo
 
+import com.alex44.digitalnomadstestapp.common.interfaces.INetworkStatus
+import com.alex44.digitalnomadstestapp.common.model.exception.OfflineException
 import com.alex44.digitalnomadstestapp.model.api.INewsSource
 import com.alex44.digitalnomadstestapp.model.dto.NewsResponseDTO
+import com.alex44.digitalnomadstestapp.model.enums.NewsType
 import io.reactivex.Maybe
+import io.reactivex.MaybeEmitter
 import io.reactivex.schedulers.Schedulers
 
-class NewsRepo(private var source: INewsSource, private val cache: INewsRepoCache) : INewsRepo {
+class NewsRepo(private var source: INewsSource, private val cache: INewsRepoCache, private val networkStatus: INetworkStatus) : INewsRepo {
 
     override fun getNews(pageNum: Int): Maybe<NewsResponseDTO> {
-        return source.getNews(pageNum)
-            .subscribeOn(Schedulers.io())
-            .map { t ->
-                t.articles?.let {
-                    cache.putNewsArticles(it)
+        if (networkStatus.isOnline()) {
+            return source.getNews(pageNum)
+                .subscribeOn(Schedulers.io())
+                .map { t ->
+                    t.articles?.let {
+                        cache.putNewsArticles(it.filter {article ->
+                            article.newsType == NewsType.NEWS
+                        })
+                    }
+                    return@map t
                 }
-                return@map t
+        }
+        else {
+            return Maybe.create { emitter: MaybeEmitter<NewsResponseDTO> ->
+                emitter.onError(OfflineException("No internet connection"))
             }
+                .subscribeOn(Schedulers.io())
+        }
     }
 }
